2d15989fecb8c26ac520b05b97b1ded6060a857a
Gianfranco Bogetti <gbogetti@dc.uba.ar>
2018-04-18 17:45:53 -0300

    QUITO INCLUDE -.-

--- codigo/filtros/monocromatizar_inf_c.c
+++ codigo/filtros/monocromatizar_inf_c.c
@@ -1,4 +1,3 @@
-#include "monocromatizar_inf.c"
 #define max(a, b) ((a)>(b))?(a):(b)
 
 void monocromatizar_inf_c(
@@ -11,32 +10,18 @@ void monocromatizar_inf_c(
 {
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
-	for (int i = 0; i < height; i = i + 4)
+	for (int i = 0; i < height; i++)
 	{
-		for (int j = 0; j < width; j = j + 4)
+		for (int j = 0; j < width; j = j+4)
 		{
-			bgra_t* pixel_src = src_matrix[i][j];
-			if(max(pixel_src->b, pixel_src->g) ==	max(pixel_src->g, pixel_src->r))
-			{
-				dst_matrix[i/4][j/4] = pixel_src->g;
-			}else{
-				dst_matrix[i/4][j/4] = max(pixel_src->b, pixel_src->r);
-			}
+			// bgra_t* pixel_src = src_matrix[i][j];
+			// if(max(pixel_src->b, pixel_src->g) ==	max(pixel_src->g, pixel_src->r))
+			// {
+			// 	dst_matrix[i/4][j/4] = pixel_src->g;
+			// }else{
+			// 	dst_matrix[i/4][j/4] = max(pixel_src->b, pixel_src->r);
+			// }
 
 		}
 	}
-
-	// for (int i = 0; i < height; i++)
-	// {
-	// 	for (int j = 0; j < width; j=j+4)
-	// 	{
-	// 		bgra_t pixel = src_matrix[i][j];
-	// 		if(max(pixel->r, pixel->b) ==	max(pixel->r, pixel->g)
-	// 		{
-	// 			dst_matrix[i/4][j/4] = pixel->r;
-	// 		}else{
-	// 			dst_matrix[i/4][j/4] = max(pixel->g,pixel->b);
-	// 		}
-	// 	}
-	// }
 }
