fd875e7a71309d584528f6481ba74b8a30cdf75d
Santiago Feliu <sfeliu@dc.uba.ar>
2018-04-23 16:50:57 -0300

    Esta pasando algo mal en monocromatizar

--- codigo/filtros/monocromatizar_inf_asm.asm
+++ codigo/filtros/monocromatizar_inf_asm.asm
@@ -57,6 +57,8 @@ monocromatizar_inf_asm:
 		pslld xmm1, 8					        ; xmm1 = |   R   |   R   |   R   |   R   |
 	
 		movdqu xmm5, xmm3						; xmm5 = xmm3
+		psrld xmm5, 8 
+		psrld xmm4, 8
 		pcmpgtd xmm5, xmm4						; xmm5 = |max(B,G)|max(B,G)|max(B,G)|max(B,G)| "Mascara"
 		pand xmm3, xmm5							; xmm3 = |   B    |   0    |    0   |    B   |
 		pxor xmm5, xmm7
@@ -64,6 +66,8 @@ monocromatizar_inf_asm:
 		por xmm3, xmm4							; xmm3 = |   B    |   G    |    G   |    B   |
 
 		movdqu xmm5, xmm3						; xmm5 = xmm3
+		psrld xmm5, 8
+		psrld xmm1, 8
 		pcmpgtd xmm5, xmm1						; xmm5 = |max(B,G,R)|max(B,G,R)|max(B,G,R)|max(B,G,R)| "Mascara"
 		pand xmm3, xmm5							; xmm3 = |     0    |     0    |     G    |     B    |
 		pxor xmm5, xmm7
@@ -80,15 +84,13 @@ monocromatizar_inf_asm:
 		por xmm1, xmm3							; xmm1 = |R|R|R|0|R|R|R|0|G|G|G|0|B|B|B|0|
 		por xmm1, xmm2							; xmm1 = |R|R|R|A|R|R|R|A|G|G|G|A|B|B|B|A|
 
-		movdqu xmm1, [rdi]
-		mov r15, 0xf0
-		movq xmm2, r15
-		paddb xmm1, xmm2
-		pxor xmm1, xmm1
 		movdqu [rsi], xmm1
 
 		add rdi, 16
 		add rsi, 16
+		dec rcx
+		cmp rcx, 0
+		jg .ciclo
 
 	pop rbp
 
