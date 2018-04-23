5f855033245097d2ace4751158f2ea745d80e4ae
Santiago Feliu <sfeliu@dc.uba.ar>
2018-04-23 15:56:41 -0300

    asm blit y mono

--- codigo/filtros/blit_asm.asm
+++ codigo/filtros/blit_asm.asm
@@ -1,7 +1,65 @@
 section .data
 DEFAULT REL
+mask1: times 4 DD 0x00FF00FF
+mask2: times 4 DD 0xFF000000
+
+;rdi = unsigned char *src
+;rsi = unsigned char *dst
+;rdx = int w
+;rcx = int h
+;r8 = int src_row_size
+;r9 = int dst_row_size
+;[rbp + 16] = unsigned char *blit
+;[rbp + 24] = int bw
+;[rbp + 32] = int bh
+;[rbp + 40] = int b_row_size
+
 section .text
+
 global blit_asm
 blit_asm:
-;COMPLETAR
-	ret
+	push rbp;
+	mov rbp, rsp;
+	mov r13, [rbp+16];
+
+	mov r14, [rbp+24];
+	mov r15, [rbp+32];
+	mov r10, rcx;
+	;mov r14, 400;
+	;mov r15, 300;
+	;mov r14, 90;
+	;mov r15, 130;
+
+	movdqu xmm3, [mask2];		xmm3 = [{00,00,00,FF},{00,00,00,FF},...]
+	movdqu xmm4, [mask1];		xmm4 = [{255,00,255,00},....]
+
+	ciclo1:
+		mov r12, rdx;
+		ciclo2:
+			movdqu xmm1, [rdi];			xmm0 = [p1|p2|p3|p4]
+			cmp r12, r14;				r12 = (w - i) < bw ?? donde i es la cantidad de columnas recorridas
+			jg continuar;
+		blit:
+			cmp rcx, r15;				rcx = (h-j) > bh ?? donde j es la cantidad de filas recorridas 
+			jg continuar;
+			movdqu xmm0, [r13];			xmm0 = [PixBlit1|PixBlit2|PixBlit3|PixBlit4]
+			pand xmm3, xmm0; 			xmm3 = [{0,0,0,alpha},{0,0,0,alpha},...]
+			por xmm3, xmm4;				xmm3 = [{255,0,255,alpha},{255,0,255,alpha},...] alpha es de PixBlit_j
+			movdqu xmm2, xmm0;			xmm2 = xmm0
+			pcmpeqd xmm0, xmm3;			pone en xmm0 0 o 1 en la pos de los pix que son iguales a los de xmm3
+			blendvps xmm2, xmm1;
+			movdqu [rsi], xmm2;
+			add r13, 16;
+			jmp continuar2;
+		continuar:
+			movdqu [rsi], xmm1;
+		continuar2:
+			add rdi, 16;
+			add rsi, 16;
+			sub r12, 4;
+			cmp r12, 0;
+			jg ciclo2;
+		loop ciclo1;					resta 1 a rcx, donde rcx = h. Luego el ciclo1 termina cuando h = 0.
+	fin:
+		pop rbp
+		ret
--- codigo/filtros/monocromatizar_inf_asm.asm
+++ codigo/filtros/monocromatizar_inf_asm.asm
@@ -19,15 +19,83 @@ extern monocromatizar_inf_c
 
 global monocromatizar_inf_asm
 
+section .rodata
+	mascara_ultimo_byte_de_DW: DB 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff
+	mover_una_posicion_DW: DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8
+	negativos times 16 DB 0xff
+
 section .text
 
 monocromatizar_inf_asm:
-	;; TODO: Implementar
 
-	sub rsp, 8
+	push rbp									; Stack frame								'ALINEADO'
+	mov rbp, rsp
 
-	call monocromatizar_inf_c
+	mov rax, rdx
+	mul rcx
+	mov rcx, rax
+	shr rcx, 2									;rcx = height*width / 4 (cantidad de loops)
 
-	add rsp, 8
+	movdqu xmm0, [mascara_ultimo_byte_de_DW]
+	pxor xmm2, xmm2
+	pxor xmm3, xmm3
+	pxor xmm4, xmm4
+	pxor xmm5, xmm5
+	movdqu xmm6, [mover_una_posicion_DW]
+	movdqu xmm7, [negativos]
+	
+	.ciclo:
+		movdqu xmm1, [rdi]						; xmm1 = |R|G|B|A|R|G|B|A|R|G|B|A|R|G|B|A| = |PIXEL|PIXEL|PIXEL|PIXEL|
+		pblendvb xmm2, xmm1						; xmm2 = |   A   |   A   |   A   |   A   |
+		
+		pslld xmm1, 8					        ; xmm1 = |-|R|G|B|-|R|G|B|-|R|G|B|-|R|G|B|
+		pblendvb xmm3, xmm1						; xmm3 = |   B   |   B   |   B   |   B   |
+
+		pslld xmm1, 8					        ; xmm1 = |-|-|R|G|-|-|R|G|-|-|R|G|-|-|R|G|
+		pblendvb xmm4, xmm1						; xmm4 = |   G   |   G   |   G   |   G   |
+
+		pslld xmm1, 8					        ; xmm1 = |   R   |   R   |   R   |   R   |
+	
+		movdqu xmm5, xmm3						; xmm5 = xmm3
+		pcmpgtd xmm5, xmm4						; xmm5 = |max(B,G)|max(B,G)|max(B,G)|max(B,G)| "Mascara"
+		pand xmm3, xmm5							; xmm3 = |   B    |   0    |    0   |    B   |
+		pxor xmm5, xmm7
+		pand xmm4, xmm5							; xmm4 = |   0    |   G    |    G   |    0   |
+		por xmm3, xmm4							; xmm3 = |   B    |   G    |    G   |    B   |
+
+		movdqu xmm5, xmm3						; xmm5 = xmm3
+		pcmpgtd xmm5, xmm1						; xmm5 = |max(B,G,R)|max(B,G,R)|max(B,G,R)|max(B,G,R)| "Mascara"
+		pand xmm3, xmm5							; xmm3 = |     0    |     0    |     G    |     B    |
+		pxor xmm5, xmm7
+		pand xmm1, xmm5							; xmm1 = |     R    |     R    |     0    |     0    |
+		por xmm3, xmm1							; xmm3 = |     R    |     R    |     G    |     B    |
+
+												; xmm3 = |0|0|0|R|0|0|0|R|0|0|0|G|0|0|0|B| "Máximos de cada pixel"
+	
+		psrld xmm3, 8							; xmm3 = |0|0|R|0|0|0|R|0|0|0|G|0|0|0|B|0|
+		movdqu xmm1, xmm3
+		psrld xmm3, 8							; xmm3 = |0|R|0|0|0|R|0|0|0|G|0|0|0|B|0|0|
+		por xmm1, xmm3							; xmm1 = |0|R|R|0|0|R|R|0|0|G|G|0|0|B|B|0|
+		psrld xmm3, 8							; xmm3 = |R|0|0|0|R|0|0|0|G|0|0|0|B|0|0|0|
+		por xmm1, xmm3							; xmm1 = |R|R|R|0|R|R|R|0|G|G|G|0|B|B|B|0|
+		por xmm1, xmm2							; xmm1 = |R|R|R|A|R|R|R|A|G|G|G|A|B|B|B|A|
+
+		movdqu xmm1, [rdi]
+		mov r15, 0xf0
+		movq xmm2, r15
+		paddb xmm1, xmm2
+		pxor xmm1, xmm1
+		movdqu [rsi], xmm1
+
+		add rdi, 16
+		add rsi, 16
+
+	pop rbp
+
+	;sub rsp, 8
+
+	;call monocromatizar_inf_c
+
+	;add rsp, 8
 
 	ret
