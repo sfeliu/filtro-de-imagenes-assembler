cafd1f0efc19498fe698426e525dae083bb07547
Gianfranco Bogetti <GBogetti@coasa.com.ar>
2018-04-24 12:24:33 -0300

    Haciendo temperature en asm y progreso con experimentacion

--- codigo/exp/GenerarImg.cpp
+++ codigo/exp/GenerarImg.cpp
@@ -2,35 +2,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <iostream>
 #include <vector>
 using namespace std;
 
 int main (){
 
-  srand(time(NULL));
+	srand(time(NULL));
 
-  vector<int> iguales;
-  iguales.open("CasosIguales.txt");
-  int i = 1;
-  int elem;
-  int mochi;
-  while(i < 26){
-    elem = i;
-    mochi = i;
-    iguales << elem << " " << mochi << endl;
-    int j = 0;
-    int peso;
-    int valor;
-    while(j < elem){
-      peso = rand() % mochi + 1;
-      valor = rand() % 100 + 1;
-      iguales << peso << " " << valor << endl;
-      j++;
-    }
-    i++;
-  }
-  iguales.close();
+	vector<int> data[512*512];
+	int i = 0;
+	int r;
+	int g;
+	int b;
+	int a = 0;
+	while(i < 512*512){
+		r = rand() % 255;
+		g = rand() % 255;
+		b = rand() % 255;
 
-  return 0;
+		data[i+0] = b;
+		data[i+1] = g;
+		data[i+2] = r;
+		data[i+3] = a;
+
+		i = i + 4;
+	}
+
+	BMPV5H* header = get_BMPV5H(512, 512);
+	*BMP img = bmp_create(header, data);
+
+	snprintf(img, sizeof(img), "img/random.bmp");
+
+	return 0;
 }
--- codigo/filtros/temperature_asm.asm
+++ codigo/filtros/temperature_asm.asm
@@ -19,14 +19,17 @@ global temperature_asm
 
 section .rodata
 
-; mascara_ultimo_byte_de_DW: DB 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0
-; mover_una_posicion_DW: DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8
-; negativos times 16 DB 0xff
+mascara_32:		DB 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0
+mascara_96:		DB 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0
+mascara_160:	DB 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0
+mascara_224:	DB 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0
+mascara_128:	DB 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0
+m_0_0_255:		DB 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0, 0xff, 0, 0, 0
 
 section .text
 
 temperature_asm:
-push rbp									; Stack frame						'ALINEADO'
+push rbp									; Stack frame			  'ALINEADO'
 mov rbp, rsp
 
 mov rax, rdx
@@ -34,58 +37,126 @@ mul rcx
 mov rcx, rax
 shr rcx, 2									;rcx = height*width / 4 (cantidad de loops)
 
-; movdqu xmm0, [mascara_ultimo_byte_de_DW]
-; movdqu xmm6, [mover_una_posicion_DW]
-; movdqu xmm7, [negativos]
+movdqu xmm11, [mascara_32]
+movdqu xmm12, [mascara_96]
+movdqu xmm13, [mascara_160]
+movdqu xmm14, [mascara_224]
+
+movdqu xmm10, [mascara_128]
+movdqu xmm9, [m_0_0_255]
 
 .ciclo:
+	pxor xmm0, xmm0
+	pxor xmm1, xmm1
 	pxor xmm2, xmm2
 	pxor xmm3, xmm3
 	pxor xmm4, xmm4
 	pxor xmm5, xmm5
+	pxor xmm6, xmm6
+	pxor xmm7, xmm7
+
 	movdqu xmm1, [rdi]						; xmm1 = |A|R|G|B|A|R|G|B|A|R|G|B|A|R|G|B| = |PIXEL|PIXEL|PIXEL|PIXEL|
-	pblendvb xmm2, xmm1						; xmm2 = |   B   |   B   |   B   |   B   |
+	pblendvb xmm0, xmm1						; xmm0 = |   B   |   B   |   B   |   B   |
 
 	psrld xmm1, 8					        ; xmm1 = |-|A|R|G|-|A|R|G|-|A|R|G|-|A|R|G|
-	pblendvb xmm3, xmm1						; xmm3 = |   G   |   G   |   G   |   G   |
+	pblendvb xmm2, xmm1						; xmm2 = |   G   |   G   |   G   |   G   |
 
 	psrld xmm1, 8					        ; xmm1 = |-|-|A|R|-|-|A|R|-|-|A|R|-|-|A|R|
-	pblendvb xmm4, xmm1						; xmm4 = |   R   |   R   |   R   |   R   |
+	pblendvb xmm3, xmm1						; xmm3 = |   R   |   R   |   R   |   R   |
 
-	psrld xmm1, 8					        ; xmm1 = |   A   |   A   |   A   |   A   |
+	paddd xmm0, xmm2						; xmm0 = |  B+G  |  B+G  |  B+G  |  B+G  |
+	paddd xmm0, xmm3						; xmm0 = | B+G+R | B+G+R | B+G+R | B+G+R |
+	;xmm0/3 (?)								; xmm0 = |   T   |   T   |   T   |   T   |
 
-	movdqu xmm5, xmm2						; xmm5 = xmm2
-	paddsw xmm5, xmm3						; xmm5 = |  B+G  |  B+G  |  B+G  |  B+G  |
-	paddsw xmm5, xmm4						; xmm5 = | B+G+R | B+G+R | B+G+R | B+G+R |
-											; xmm5 = |   T   |   T   |   T   |   T   |
+	movdqu xmm1, xmm0						; xmm1 = xmm0
+	movdqu xmm2, xmm0						; xmm2 = xmm0
+	movdqu xmm3, xmm0						; xmm3 = xmm0
+	movdqu xmm4, xmm0						; xmm4 = xmm0
 
 
+	pcmpgtd xmm1, xmm11						; xmm1 = |  T>69  |  T>69  |  T>69  |  T>69  | "Mascara"
+	;not xmm1								; xmm1 = | 0<T<69 | 0<T<69 | 0<T<69 | 0<T<69 | "Mascara"
+
+	movdqu xmm5, xmm0						; xmm5 = xmm0
+	paddd xmm5, xmm5
+	paddd xmm5, xmm5
+	paddd xmm5, xmm5						; xmm5 = |  T*4   |  T*4   |  T*4   |  T*4   |
+	paddd xmm5, xmm10						; xmm5 = |T*4+128 |T*4+128 |T*4+128 |T*4+128 | [0|0|0|T*4+128](PIXEL)
+
+	pand xmm1, xmm5							;
 
 
+	pcmpgtd xmm2, xmm12						; xmm2 = |  T<96  |  T>96  |  T>96  |  T>96  | "Mascara"
+	;not xmm2								; xmm2 = | 0<T<96 | 0<T<96 | 0<T<96 | 0<T<96 | "Mascara"
+	pxor xmm2, xmm1							; xmm2 = |69<=T<96|69<=T<96|69<=T<96|69<=T<96| "Mascara"
 
-	; movdqu xmm5, xmm3						; xmm5 = xmm3
-	; pcmpgtd xmm5, xmm4						; xmm5 = |max(R,G)|max(R,G)|max(R,G)|max(R,G)| "Mascara"
-	; pand xmm3, xmm5							; xmm3 = |   R    |   0    |    0   |    R   |
-	; pxor xmm5, xmm7
-	; pand xmm4, xmm5							; xmm4 = |   0    |   G    |    G   |    0   |
-	; por xmm3, xmm4							; xmm3 = |   R    |   G    |    G   |    R   |
-	;
-	; movdqu xmm5, xmm3						; xmm5 = xmm3
-	; pcmpgtd xmm5, xmm2						; xmm5 = |max(B,G,R)|max(B,G,R)|max(B,G,R)|max(B,G,R)| "Mascara"
-	; pand xmm3, xmm5							; xmm3 = |     0    |     0    |     G    |     R    |
-	; pxor xmm5, xmm7
-	; pand xmm2, xmm5							; xmm2 = |     B    |     B    |     0    |     0    |
-	; por xmm3, xmm2							; xmm3 = |     B    |     B    |     G    |     R    |
-	;
-	; 										; xmm3 = |0|0|0|B|0|0|0|B|0|0|0|G|0|0|0|R| "Máximos de cada pixel"
+	movdqu xmm5, xmm0						; xmm5 = xmm0
+	psubd xmm5, xmm11						; xmm5 = |  T-32  |  T-32  |  T-32  |  T-32  |
+	paddd xmm5, xmm5
+	paddd xmm5, xmm5
+	paddd xmm5, xmm5						; xmm5 = |(T-32)*4|(T-32)*4|(T-32)*4|(T-32)*4| [0|0|0|(T-32)*4](PIXEL)
+	pslld xmm5, 8							; [0|0|(T-32)*4|0](PIXEL)
+	paddd xmm5, xmm9						; [0|0|(T-32)*4|255](PIXEL)
 
-	movdqu xmm2, xmm3
-	pslld xmm3, 8							; xmm3 = |0|0|R|0|0|0|R|0|0|0|G|0|0|0|B|0|
-	por xmm2, xmm3							; xmm2 = |0|0|R|R|0|0|R|R|0|0|G|G|0|0|B|B|
-	pslld xmm3, 8							; xmm3 = |0|R|0|0|0|R|0|0|0|G|0|0|0|B|0|0|
-	por xmm2, xmm3							; xmm2 = |0|R|R|R|0|R|R|R|0|G|G|G|0|B|B|B|
-	pslld xmm1, 24							; xmm1 = |A|0|0|0|A|0|0|0|A|0|0|0|A|0|0|0|
-	por xmm2, xmm1							; xmm2 = |A|R|R|R|A|R|R|R|A|G|G|G|A|B|B|B|
+	pand xmm2, xmm5							;
+
+
+	pcmpgtd xmm3, xmm13						; xmm3 = |  T>160 |  T>160 |  T>160 |  T>160 | "Mascara"
+	;not xmm3								; xmm3 = | 0<T<160| 0<T<160| 0<T<160| 0<T<160| "Mascara"
+	pxor xmm3, xmm2							; xmm3 = |96<=T<160|96<=T<160|96<=T<160|96<=T<160| "Mascara"
+
+	movdqu xmm5, xmm0						; xmm5 = xmm0
+	psubd xmm5, xmm12						; xmm5 = |  T-96  |  T-96  |  T-96  |  T-96  |
+	paddd xmm5, xmm5
+	paddd xmm5, xmm5
+	paddd xmm5, xmm5						; xmm5 = |(T-96)*4|(T-96)*4|(T-96)*4|(T-96)*4| [0|0|0|(T-96)*4](PIXEL)
+	movdqu xmm6, xmm5						; xmm5 = xmm5
+	pslld xmm5, 8							; [0|0|(T-96)*4|0](PIXEL)
+	paddd xmm5, xmm9						; [0|0|(T-96)*4|255](PIXEL)
+	pslld xmm5, 8							; [0|(T-96)*4|255|0](PIXEL)
+	paddd xmm5, xmm9						; [0|(T-96)*4|255|255](PIXEL)
+	psubd xmm5, xmm6						; [0|(T-96)*4|255|255-(T-96)*4](PIXEL)
+
+	pand xmm3, xmm5							;
+
+
+	pcmpgtd xmm4, xmm14						; xmm4 = |  T>224 |  T>224 |  T>224 |  T>224 | "Mascara"
+	movdqu xmm7, xmm4
+	;not xmm4								; xmm4 = | 0<T<224| 0<T<224| 0<T<224| 0<T<224| "Mascara"
+	pxor xmm4, xmm3							; xmm4 = |160<=T<224|160<=T<224|160<=T<224|160<=T<224| "Mascara"
+
+	movdqu xmm5, xmm0						; xmm5 = xmm0
+	psubd xmm5, xmm13						; xmm5 = |  T-160 |  T-160 |  T-160 |  T-160 |
+	paddd xmm5, xmm5
+	paddd xmm5, xmm5
+	paddd xmm5, xmm5						; xmm5 = |(T-160)*4|(T-160)*4|(T-160)*4|(T-160)*4| [0|0|0|(T-96)*4](PIXEL)
+	movdqu xmm6, xmm5						; xmm6 = xmm5
+	movdqu xmm5, xmm9						; xmm5 = xmm9
+	pslld xmm5, 8							; [0|0|255|0](PIXEL)
+	paddd xmm5, xmm9						; [0|0|255|255](PIXEL)
+	psubd xmm5, xmm6						; [0|0|255|255-(T-160)*4](PIXEL)
+	pslld xmm5, 8							;[0|255|255-(T-160)*4|0](PIXEL)
+
+	pand xmm4, xmm5							;
+
+
+	movdqu xmm5, xmm0						; xmm5 = xmm0
+	psubd xmm5, xmm14						; xmm5 = |  T-224 |  T-224 |  T-224 |  T-224 |
+	paddd xmm5, xmm5
+	paddd xmm5, xmm5
+	paddd xmm5, xmm5						; xmm5 = |(T-224)*4|(T-224)*4|(T-224)*4|(T-224)*4| [0|0|0|(T-224)*4](PIXEL)
+	movdqu xmm6, xmm5						; xmm6 = xmm5
+	movdqu xmm5, xmm9						; xmm5 = xmm9
+	psubd xmm5, xmm6						; [0|0|0|255-(T-224)*4](PIXEL)
+	pslld xmm5, 16							; [0|255-(T-224)*4|0|0](PIXEL)
+
+	pand xmm7, xmm5							;
+
+
+	por xmm2, xmm1
+	por xmm2, xmm3
+	por xmm2, xmm4
+	por xmm2, xmm7
 
 	movdqu [rsi], xmm2
 
