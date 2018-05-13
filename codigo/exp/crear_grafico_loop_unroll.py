10702fafe098b748442a663eb4b57c757879c9fc
Gianfranco Bogetti <gbogetti@dc.uba.ar>
2018-05-13 18:01:55 -0300

    Loop Unroll exp terminada

--- codigo/exp/crear_grafico_loop_unroll.py
+++ codigo/exp/crear_grafico_loop_unroll.py
@@ -56,4 +56,4 @@ plt.xlabel("Tamaño de la imagen")
 plt.ylabel("Cantidad de miles de ciclos de clock")
 plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
 		   ['1', '2', '4', '8', '16', '32', '64', '128', '256', '512', '1024', '2048'])
-plt.savefig("Exp_MonoXY.png")
+plt.savefig("ExpLoopUnroll.png")
